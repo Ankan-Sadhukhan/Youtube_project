85ed1652ca501fd3acfd33b86783818df278f473
Ankan-Sadhukhan <sadhukhanankan7@gmail.com>
2023-11-22 19:31:30 +0530

    build n-level comments

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { closeMenu } from '../utils/appSlice';
 import { useSearchParams } from 'react-router-dom';
+import CommentsContainer from './CommentsContainer';
 
 const WatchPage = () => {
 
@@ -13,6 +14,7 @@ const WatchPage = () => {
         dispatch( closeMenu());
     },[])
   return (
+    <div>
     <div className='mx-5 p-2'><iframe width="900" 
     height="460" 
     src={"https://www.youtube.com/embed/"+searchParams.get("v")} 
@@ -21,6 +23,8 @@ const WatchPage = () => {
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
     allowFullScreen>
     </iframe>
+    </div>
+    <CommentsContainer/>
     </div>
   )
 }
